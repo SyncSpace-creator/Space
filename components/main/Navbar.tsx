e376bce6bb811e441a781010d2721b779a6de6e2
SyncSpace-creator <malakaremon2007@gmail.com>
2025-05-05 09:31:43 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar = () => {
           <Image
             src="/NavLogo1.jpeg"
             alt="logo"
-            width={70}
-            height={70}
+            width={60}
+            height={60}
             className="cursor-pointer hover:animate-slowspin"
           />
 
