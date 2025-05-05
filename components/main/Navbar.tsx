206a74f8ae9a2420ea0ea29aa12fdd4acd150914
SyncSpace-creator <malakaremon2007@gmail.com>
2025-05-05 09:28:52 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
-            src="/NavLogo.jpeg"
+            src="/NavLogo1.jpeg"
             alt="logo"
             width={70}
             height={70}
